87c21b40a1825c418782e1e53119832a44a40426
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-06-15 20:38:54 +0200

    Récupération des travaux avec fetch et affichage sur la page d'accueil

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,5 +1,16 @@
-// Récupérer les travaux avec fetch
+// Récupérer les travaux avec fetch et les afficher
+let gallery = document.querySelector(".gallery");
+console.log(gallery);
 
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) =>
+    data.map((e) => {
+      gallery.innerHTML += ` <figure>
+  <img src="${e.imageUrl}" alt="${e.title}">
+  <figcaption>${e.title}</figcaption>
+</figure>
+`;
+    })
+  );
+
