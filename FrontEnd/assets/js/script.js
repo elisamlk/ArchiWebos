bec2da0dfdfa0249470f902f5ef02b5f1f3cac8d
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-06-16 17:00:08 +0200

    Ajout de la fonction filtre en javascript

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,15 +1,15 @@
 // Récupérer les travaux avec fetch et les afficher
 let gallery = document.querySelector(".gallery");
 
-// fetch("http://localhost:5678/api/works")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
   .then((data) =>
     data.map((e) => {
-      gallery.innerHTML += ` <figure data-filter="${e.category.name}">
+      gallery.innerHTML += ` <figure class="project" data-filter="${e.category.name}">
   <img src="${e.imageUrl}" alt="${e.title}">
   <figcaption>${e.title}</figcaption>
 </figure>
@@ -17,5 +17,33 @@ fetch("http://localhost:5678/api/works")
     })
   );
 
-  
+// Filtrer les photos selon le thème
 
+let filters = document.querySelectorAll(".filter");
+
+filters.forEach((filter) => {
+  filter.addEventListener("click", function () {
+    let selectedFilter = filter.getAttribute("data-category");
+    let itemsToHide = document.querySelectorAll(
+      `.project:not([data-filter='${selectedFilter}'])`
+    );
+    let itemsToShow = document.querySelectorAll(
+      `.project[data-filter='${selectedFilter}']`
+    );
+
+    if (selectedFilter == "tous") {
+      itemsToHide = [];
+      itemsToShow = document.querySelectorAll(".project[data-filter]");
+    }
+
+    itemsToHide.forEach((el) => {
+      el.classList.add("hide");
+      // el.classList.remove("show");
+    });
+
+    itemsToShow.forEach((el) => {
+      el.classList.remove("hide");
+      // el.classList.add("show");
+    });
+  });
+});
