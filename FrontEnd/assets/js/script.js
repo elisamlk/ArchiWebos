5d7968590c3c646a58b4ff119e7580a00037e9a1
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-06-16 14:42:22 +0200

    Ajout des filtres en html

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,16 +1,21 @@
 // Récupérer les travaux avec fetch et les afficher
 let gallery = document.querySelector(".gallery");
-console.log(gallery);
+
+// fetch("http://localhost:5678/api/works")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
 
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
   .then((data) =>
     data.map((e) => {
-      gallery.innerHTML += ` <figure>
+      gallery.innerHTML += ` <figure data-filter="${e.category.name}">
   <img src="${e.imageUrl}" alt="${e.title}">
   <figcaption>${e.title}</figcaption>
 </figure>
 `;
     })
   );
+
+  
 
