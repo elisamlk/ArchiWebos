1238afa81bda99d30ac0d9bd1b8a2d982e18953b
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-06-23 19:31:02 +0200

    Fix le problème de filtre

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,9 +1,20 @@
+// Récupérer les catégories
+let categoryBtn = document.querySelector(".category-btn");
+let filters;
+
+fetch("http://localhost:5678/api/categories")
+  .then((response) => response.json())
+  .then((data) => {
+    data.map((e) => {
+      categoryBtn.innerHTML += `<li class="filter" data-category="${e.name}">${e.name}</li>
+`;
+    });
+    filters = document.querySelectorAll(".filter");
+    categoryFilter(filters);
+  });
+
 // Récupérer les travaux avec fetch et les afficher
 let gallery = document.querySelector(".gallery");
-
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
 
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
@@ -17,48 +28,39 @@ fetch("http://localhost:5678/api/works")
     })
   );
 
-// Récupérer les catégories
+function categoryFilter(filters) {
+  // let filters = document.querySelectorAll(".filter");
+  console.log(filters);
 
-let categoryBtn = document.querySelector(".category-btn");
-console.log(categoryBtn);
+  filters.forEach((filter) => {
+    filter.addEventListener("click", function () {
+      let selectedFilter = filter.getAttribute("data-category");
+      let itemsToHide = document.querySelectorAll(
+        `.project:not([data-filter='${selectedFilter}'])`
+      );
+      let itemsToShow = document.querySelectorAll(
+        `.project[data-filter='${selectedFilter}']`
+      );
 
-fetch("http://localhost:5678/api/categories")
-  .then((response) => response.json())
-  .then((data) =>
-    data.map((e) => {
-      categoryBtn.innerHTML += `  <li class="filter" data-category="${e.name}">${e.name}</li>
-`;
-    })
-  );
+      if (selectedFilter == "tous") {
+        itemsToHide = [];
+        itemsToShow = document.querySelectorAll(".project[data-filter]");
+      }
 
-// Filtrer les photos selon le thème
+      itemsToHide.forEach((el) => {
+        el.classList.add("hide");
+        // el.classList.remove("show");
+      });
 
-let filters = document.querySelectorAll(".filter");
-console.log(filters);
-
-filters.forEach((filter) => {
-  filter.addEventListener("click", function () {
-    let selectedFilter = filter.getAttribute("data-category");
-    let itemsToHide = document.querySelectorAll(
-      `.project:not([data-filter='${selectedFilter}'])`
-    );
-    let itemsToShow = document.querySelectorAll(
-      `.project[data-filter='${selectedFilter}']`
-    );
-
-    if (selectedFilter == "tous") {
-      itemsToHide = [];
-      itemsToShow = document.querySelectorAll(".project[data-filter]");
-    }
-
-    itemsToHide.forEach((el) => {
-      el.classList.add("hide");
-      // el.classList.remove("show");
-    });
-
-    itemsToShow.forEach((el) => {
-      el.classList.remove("hide");
-      // el.classList.add("show");
+      itemsToShow.forEach((el) => {
+        el.classList.remove("hide");
+        // el.classList.add("show");
+      });
     });
   });
-});
+}
+
+// dans le then du fetch post
+localStorage.setItem("token", "1234");
+
+//logout
